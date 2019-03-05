4b2515f870d9f3ee896069ba67f478787b15ddb0
ShuHua <pitsitansom@gmail.com>
2019-03-04 19:13:51 -0600

    connected

---
+++ flask-backend/static/react/precache-manifest.e369fffea61d4fdfb75dda83707f1f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "450a472931a815e8e4c5",
+    "url": "/static/react/js/runtime~main.450a4729.js"
+  },
+  {
+    "revision": "31b5c32ca673773d331b",
+    "url": "/static/react/js/main.31b5c32c.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/static/react/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "31b5c32ca673773d331b",
+    "url": "/static/react/css/main.4a884dd2.chunk.css"
+  },
+  {
+    "revision": "931e0d108945c02725f92ee6ce6a0a9c",
+    "url": "/static/react/../../templates/index.html"
+  }
+];
