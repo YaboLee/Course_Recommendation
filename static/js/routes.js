dba0b2e739ef7d7b442e3953432db606085d71ff
Yabo Li <yabolee96@gmail.com>
2019-03-18 22:50:28 -0400

    update login form

--- static/js/routes.js
+++ static/js/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Route, hashHistory } from 'react-router-dom';
 import Home from './components/home';
 import Register from './components/register/register';
+import Login from './components/login/login'
 
 // import more components
 export default (
@@ -9,6 +10,7 @@ export default (
      <div>
       <Route exact path='/' component={Home} />
       <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
      </div>
     </HashRouter>
 );
