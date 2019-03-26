2ed2a149cf2806bcf91f9cc5abe4a4b8e58ed1e4
Xinhang Chen <1417798061@qq.com>
2019-03-25 22:39:34 -0500

    basic structure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request,g
 from flask_cors import CORS
-
+from db import get_db
 app = Flask(__name__, static_folder="./static", template_folder="./templates")
 app.config.from_mapping(
         SECRET_KEY='dev'
@@ -10,7 +10,13 @@ cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route("/")
 def hello():
-	return render_template("index.html")
+    # print(g.username)
+    # username = g.username
+    # db = get_db()
+    # cursor = db.cursor()
+    # cursor.execute("SELECT * FROM Courses2")
+    # data = cursor.fetchall()
+    return render_template('index')
 
 # @app.route("/register", methods=('GET', 'POST'))
 # def register():
