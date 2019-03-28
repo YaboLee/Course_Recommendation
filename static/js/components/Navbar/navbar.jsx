02241408ac998dcaeb0295a53698691f1a8e57f8
Xinhang Chen <1417798061@qq.com>
2019-03-28 00:03:40 -0500

    1

--- static/js/components/Navbar/navbar.jsx
+++ static/js/components/Navbar/navbar.jsx
@@ -9,39 +9,13 @@ import {
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "../../../css/home/home.css"
 
-class Navbar extends Component {
+class Navbar2 extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
-        </Navbar>
-        <br />
-        <Navbar bg="primary" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-light">Search</Button>
-          </Form>
-        </Navbar>
-
-        <br />
-        <Navbar bg="light" variant="light">
+        <Navbar bg="nav" variant="light">
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="mr-auto">
             <Nav.Link href="#home">Home</Nav.Link>
@@ -58,4 +32,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default Navbar2;
